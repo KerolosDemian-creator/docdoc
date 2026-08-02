c7fc4fa62de29a1fd6c7c28717a9f8f7dae51a13
KerolosDemian-creator <kerolosdemian298@gmail.com>
2026-08-02 15:33:43 +0300

    environment settup and create onboaridng screen

---
+++ lib/doc_app.dart
@@ -0,0 +1,30 @@
+import 'package:docdoc/core/routing/app_router.dart';
+import 'package:docdoc/core/routing/routes.dart';
+import 'package:docdoc/core/theme/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil_plus/flutter_screenutil_plus.dart';
+
+class DocApp extends StatelessWidget {
+  const DocApp({super.key, required this.appRouter});
+  final AppRouter appRouter;
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilPlusInit(
+      designSize: const Size(375, 812),
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'DocDoc',
+          theme: ThemeData(
+            primaryColor: AppColors.mainBlue,
+            scaffoldBackgroundColor: Colors.white,
+          ),
+          initialRoute: Routes.onboarding,
+          onGenerateRoute: appRouter.generateRoute,
+        );
+      },
+    );
+  }
+}
